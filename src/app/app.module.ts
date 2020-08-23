efbde58f4f367201e31ec77c9f7e7ac895a059ca
AbugaAroni <abugaaroni@gmail.com>
2020-08-23 23:39:37 +0300

    add the highlight dirctive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
-    QuotesDetailsComponent
+    QuotesDetailsComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
