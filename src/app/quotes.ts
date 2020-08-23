ec5c3e9455ee6c8556321d43a7b337588951a72d
AbugaAroni <abugaaroni@gmail.com>
2020-08-23 22:57:23 +0300

    add a working show how details button

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,9 +1,13 @@
 export class Quotes {
-  id: number;
-  name: string;
-  description: string;
-  author: string;
-  submitter: string;
+  showDescription: boolean;
   upvote: number;
   downvote: number;
+
+  constructor(public id: number,public name: string,public description: string, public author: string,
+    public submitter: string){
+    this.showDescription=false;
+    this.upvote=0;
+    this.downvote=0;
+  }
+
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,13 +9,17 @@ import { Quotes } from '../quotes';
 export class QuotesComponent implements OnInit {
 
   quotes:Quotes[] = [
-     {id:1,name: 'Motivation', description:'Dont drink milk after 5', author: 'Shen Tzu', submitter:'Abuga', upvote:0, downvote:0},
-     {id:2,name: 'Power', description:'Buy Cookies  for every not just yourself', author: 'Shen Tzu', submitter:'Abuga', upvote:0, downvote:0},
-     {id:3,name: 'Wisdom', description:'Get lit only on weekends', author: 'Abuga', submitter:'Abuga', upvote:0, downvote:0},
-     {id:4,name: 'Strength', description:'The way forward is always the way', author: 'John', submitter:'Abuga', upvote:0, downvote:0},
-     {id:5,name: 'Humility', description:'Though life is tough, it can always be tougher', author: 'Liz Tzu', submitter:'Abuga', upvote:0, downvote:0},
-     {id:6,name: 'Wealth', description:'No smoke, no fires', author: 'Liam Tzu', submitter:'Abuga', upvote:0, downvote:0},
+     new Quotes(1, 'Motivation', 'Dont drink milk after 5', 'Shen Tzu','Abuga'),
+     new Quotes(2, 'Power', 'Buy Cookies  for every not just yourself', 'Shen Tzu', 'Abuga'),
+     new Quotes(3, 'Wisdom', 'Get lit only on weekends', 'Abuga','Abuga'),
+     new Quotes(4, 'Strength', 'The way forward is always the way', 'John', 'Abuga'),
+     new Quotes(5, 'Humility', 'Though life is tough, it can always be tougher', 'Liz Tzu', 'Abuga'),
+     new Quotes(6, 'Wealth', 'No smoke, no fires', 'Liam Tzu', 'Abuga'),
    ];
+
+   toggleDetails(index){
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
 
   constructor() { }
 
