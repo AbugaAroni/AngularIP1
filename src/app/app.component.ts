f1cc46fe769fd90422693c67b1ff032dddca6852
AbugaAroni <abugaaroni@gmail.com>
2020-08-23 20:28:59 +0300

    create qoutes colass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quotes } from './quotes';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularIP1';
+  quotes:Quotes[] = [
+     {id:1,description:'Dont drink milk after 5', author: 'Shen Tzu'},
+     {id:2,description:'Buy Cookies  for every not just yourself', author: 'Shen Tzu'},
+     {id:3,description:'Get lit only on weekends', author: 'Abuga'},
+     {id:4,description:'The way forward is always the way', author: 'John'},
+     {id:5,description:'Though life is tough, it can always be tougher', author: 'Liz Tzu'},
+     {id:6,description:'No smoke, no fires', author: 'Liam Tzu'},
+   ];
+
 }
